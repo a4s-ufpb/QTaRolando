7d8c0d0d105be1e03899b0573728bb692bb1a50d
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2020-07-01 16:34:47 -0300

    Updated design

--- lib/styleguide.dart
+++ lib/styleguide.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-final TextStyle fadedTextStyle = TextStyle(
-  fontSize: 16,
+final TextStyle fadedTextStyle = GoogleFonts.sourceSansPro(
+  fontSize: 14,
   fontWeight: FontWeight.bold,
   color: Color(0x99FFFFFF),
 );
 
-final TextStyle whiteHeadingTextStyle = TextStyle(
+final TextStyle whiteHeadingTextStyle = GoogleFonts.montserrat(
   fontSize: 40,
   fontWeight: FontWeight.bold,
   color: Color(0xFFFFFFFF),
 );
 
-final TextStyle categoryTextStyle = TextStyle(
-  fontSize: 14,
+final TextStyle categoryTextStyle = GoogleFonts.sourceSansPro(
+  fontSize: 11,
   fontWeight: FontWeight.bold,
   color: Color(0xFFFFFFFF),
 );
@@ -22,26 +23,26 @@ final TextStyle selectedCategoryTextStyle = categoryTextStyle.copyWith(
   color: Color(0xFF13A8AE),
 );
 
-final TextStyle eventTitleTextStyle = TextStyle(
+final TextStyle eventTitleTextStyle = GoogleFonts.sourceSansPro(
   fontSize: 24,
   fontWeight: FontWeight.bold,
   color: Color(0xFF000000),
 );
 
-final TextStyle eventwhiteTitleTextStyle = TextStyle(
+final TextStyle eventwhiteTitleTextStyle = GoogleFonts.sourceSansPro(
   fontSize: 38,
   fontWeight: FontWeight.bold,
   color: Color(0xFFFFFFFF),
 );
 
-final TextStyle eventLocationTextStyle = TextStyle(
-  fontSize: 20,
+final TextStyle eventLocationTextStyle = GoogleFonts.sourceSansPro(
+  fontSize: 18,
   fontWeight: FontWeight.w800,
   color: Color(0xFF000000),
 );
 
-final TextStyle punchLine1TextStyle = TextStyle(
-  fontSize: 28,
+final TextStyle punchLine1TextStyle = GoogleFonts.montserrat(
+  fontSize: 24,
   fontWeight: FontWeight.w800,
   color: Color(0xFF13A8AE),
 );
