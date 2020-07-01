ea1e9c8ffb0460f0a59eaaaa2c2cf5f2b3135b64
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2020-07-01 16:39:49 -0300

    Class responsible for communicating with the MySql database

---
+++ lib/services/services.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:local_events/models/event.dart';
+
+import 'package:http/http.dart' as http;
+
+class Services {
+  static const ROOT = 'php file address of your mysql database';
+  static const _GET_ALL_ACTION = 'GET_ALL';
+
+  static Future<List<Evento>> getEventos() async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _GET_ALL_ACTION;
+      final response = await http.post(ROOT, body: map);
+      if (response.statusCode == 200) {
+        List<Evento> list = parseResponse(response.body);
+        return list;
+      } else {
+        return List<Evento>();
+      }
+    } catch (e) {
+      return List<Evento>();
+    }
+  }
+
+  static List<Evento> parseResponse(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Evento>((json) => Evento.fromJson(json)).toList();
+  }
+}
