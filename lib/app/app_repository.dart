f689720fdda9d7b3181f2a23e0c1ddfa14efef4c
Ronellyson <ronellysonjulio@gmail.com>
2021-08-27 17:55:20 -0300

    Updating Server Address

--- lib/app/app_repository.dart
+++ lib/app/app_repository.dart
@@ -6,7 +6,7 @@ class AppRepository{
   Future<List<Evento>> getEventos() async {
     Dio dio = new Dio();
     
-    var response = await dio.get("https://qtarolando-api.herokuapp.com/api/listar-todos");
+    var response = await dio.get("[Endereco da Api]/api/listar-todos");
     dio.close(force: true);
     List<Evento> listaEventos = Evento.fromJsonList(response.data) ;
 
